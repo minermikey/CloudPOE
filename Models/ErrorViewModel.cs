777560545d942ca8df80652dde9c29f437897cac
minermikey <michaelsingh12345678910@gmail.com>
2024-03-30 09:55:39 +0200

    First Push Of the website

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CLVD_POE_ST10273388.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Images.cs
@@ -0,0 +1,11 @@
+﻿namespace CLVD_POE_ST10273388.Models
+{
+    public class Images
+    {
+
+        public int ImageTd { get; set; }
+        public string Title { get; set; }
+        public string ImageName { get; set; }
+
+    }
+}
